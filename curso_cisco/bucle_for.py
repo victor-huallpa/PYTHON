22c957f5779d61beb1eced7e8c3a09bf1014acd2
Victor H <vech.600@gmail.com>
2023-07-31 14:09:17 -0500

    bucle for

---
+++ curso_cisco/bucle_for.py
@@ -0,0 +1,18 @@
+import time
+
+# Escribe un bucle for que cuente hasta cinco.
+num= [1,2,3,4,5]
+for i in num:
+    print(f'{i} Mississippi')
+    time.sleep(1)
+print('________________________')
+print('listos  o no, alli voy')
+print('________________________')
+
+    
+for i in range(1,6):
+    print (f'{i} Mississippi')
+    time.sleep(1)
+print('________________________')
+print('listos  o no, alli voy')
+print('________________________')
